d498cd63043dcdb6faf77d46d5db670602659347
Yufeng Liu Reach <yufeng8@vt.edu>
2017-11-17 16:53:16 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(int)
   if int%3==0
-    puts "Fizz"
+    "Fizz"
   elsif int%5==0
-    puts "Buzz"
+    "Buzz"
   elsif int%3==0 and int%5==0
-    puts "FizzBuzz"
+    "FizzBuzz"
   else
-    puts nil
+    nil
   end
 end
